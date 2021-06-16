c6ceab94059dc9204da2eaa6f6dd11a26cf50c53
ulr0 <ulr0xb@gmail.com>
2021-06-16 14:15:45 +0900

    음양 더하기

--- 210616_programmers_76501.py
+++ 210616_programmers_76501.py
@@ -1,3 +1,8 @@
+"""
+어떤 정수들이 있습니다. 이 정수들의 절댓값을 차례대로 담은 정수 배열 absolutes와 이 정수들의 부호를 차례대로 담은 불리언 배열 signs가 매개변수로 주어집니다. 실제 정수들의 합을 구하여 return 하도록 solution 함수를 완성해주세요.
+"""
+
+# 처음 풀이
 def solution(absolutes, signs):
     list = []
     for pair in zip(absolutes, signs):
@@ -9,5 +14,16 @@ def solution(absolutes, signs):
     answer = 0
     for i in list:
         answer += i
+    
+    return answer
+
+# 수정
+def solution(absolutes, signs):
+    answer = 0
+    for i, j in zip(absolutes, signs):
+        if j is True:
+            answer += i
+        else:
+            answer -= i
     
     return answer
