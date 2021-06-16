a71651d98024cb56588f6f28d7276c55fc0ff70b
ulr0 <ulr0xb@gmail.com>
2021-06-16 14:43:30 +0900

    약수의 개수와 덧셈

--- 210616_programmers_77884.py
+++ 210616_programmers_77884.py
@@ -1,7 +1,12 @@
 """
-두 정수 left와 right가 매개변수로 주어집니다. left부터 right까지의 모든 수들 중에서, 약수의 개수가 짝수인 수는 더하고, 약수의 개수가 홀수인 수는 뺀 수를 return 하도록 solution 함수를 완성해주세요.
+두 정수 left와 right가 매개변수로 주어집니다. 
+left부터 right까지의 모든 수들 중에서, 
+약수의 개수가 짝수인 수는 더하고, 
+약수의 개수가 홀수인 수는 뺀 수를 return 하도록 
+solution 함수를 완성해주세요.
 """
 
+# 처음 코드
 def solution(left, right):
     answer = 0
     for i in range(left, right+1):
@@ -13,4 +18,14 @@ def solution(left, right):
             answer += i
         else:
             answer -= i
+    return answer
+
+# 수정
+def solution(left, right):
+    answer = 0
+    for i in range(left, right+1):
+        if int(i**0.5) == i**0.5:
+            answer -= i
+        else:
+            answer += i
     return answer
