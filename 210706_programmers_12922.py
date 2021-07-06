1d2e9981d2359e7b1bcf32dcb23b4122da6dc24b
ulr0 <ulr0xb@gmail.com>
2021-07-06 16:04:44 +0900

    수박

---
+++ 210706_programmers_12922.py
@@ -0,0 +1,16 @@
+"""
+길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요. 
+예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
+"""
+
+# 처음 풀이
+def solution(n):
+    answer = '수박' * (n//2)
+    if n % 2 == 1:
+        answer += '수'
+    return answer
+
+# if문 삭제
+def solution(n):
+    answer = '수박' * (n//2) + '수' * (n%2)
+    return answer
